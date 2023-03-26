ca8f0387e21026a3771104281f5ac6cb54a5fe65
Nanda Abi Fahmi <nafal.abi@gmail.com>
2023-03-26 13:41:19 +0700

    fix: no default export page

--- src/pages/sumbatan.tsx
+++ src/pages/sumbatan.tsx
@@ -1,1 +1,9 @@
-export {};
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+
+const Page = () => {
+  return <div />
+};
+
+Page.getLayout = (page: any) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
